66e907d98f489a531c03b5086f68ec17d4fc7183
Theo Chino <githut@theochino.com>
2020-10-03 17:19:54 -0400

    First Commit

---
+++ Canvas/Canvas/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Canvas
+//
+//  Created by Theo Chino on 10/3/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+
+}
+
---
+++ Canvas/Canvas/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Canvas
+//
+//  Created by Theo Chino on 10/3/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+   
+
+
+}
+
